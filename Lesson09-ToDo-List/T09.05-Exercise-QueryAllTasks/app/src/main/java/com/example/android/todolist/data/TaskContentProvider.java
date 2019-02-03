c8725415fe90de89912c60397d3dbcd22a67fbcd
iBjors <alex.bekhelev@gmail.com>
2019-02-04 02:18:10 +0300

    T09.05 Implement ContentResolver.query(). By my own: Add ContentObserver to reload data only if it changes.

--- Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ Lesson09-ToDo-List/T09.05-Exercise-QueryAllTasks/app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -1,18 +1,18 @@
 /*
-* Copyright (C) 2016 The Android Open Source Project
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.example.android.todolist.data;
 
@@ -29,24 +29,27 @@ import android.support.annotation.NonNull;
 import static com.example.android.todolist.data.TaskContract.TaskEntry.TABLE_NAME;
 
 // Verify that TaskContentProvider extends from ContentProvider and implements required methods
-public class TaskContentProvider extends ContentProvider {
-
+public class TaskContentProvider extends ContentProvider
+{
+    
     // Define final integer constants for the directory of tasks and a single item.
     // It's convention to use 100, 200, 300, etc for directories,
     // and related ints (101, 102, ..) for items in that directory.
     public static final int TASKS = 100;
     public static final int TASK_WITH_ID = 101;
-
+    
     // CDeclare a static variable for the Uri matcher that you construct
     private static final UriMatcher sUriMatcher = buildUriMatcher();
-
+    
     // Define a static buildUriMatcher method that associates URI's with their int match
+    
     /**
-     Initialize a new matcher object without any matches,
-     then use .addURI(String authority, String path, int match) to add matches
+     * Initialize a new matcher object without any matches,
+     * then use .addURI(String authority, String path, int match) to add matches
      */
-    public static UriMatcher buildUriMatcher() {
-
+    public static UriMatcher buildUriMatcher()
+    {
+        
         // Initialize a UriMatcher with no matches by passing in NO_MATCH to the constructor
         UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
@@ -57,47 +60,53 @@ public class TaskContentProvider extends ContentProvider {
          */
         uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS, TASKS);
         uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS + "/#", TASK_WITH_ID);
-
+        
         return uriMatcher;
     }
-
+    
     // Member variable for a TaskDbHelper that's initialized in the onCreate() method
     private TaskDbHelper mTaskDbHelper;
-
+    
     /* onCreate() is where you should initialize anything you’ll need to setup
     your underlying data source.
     In this case, you’re working with a SQLite database, so you’ll need to
     initialize a DbHelper to gain access to it.
      */
     @Override
-    public boolean onCreate() {
+    public boolean onCreate()
+    {
         // Complete onCreate() and initialize a TaskDbhelper on startup
         // [Hint] Declare the DbHelper as a global variable
-
+        
         Context context = getContext();
         mTaskDbHelper = new TaskDbHelper(context);
         return true;
     }
-
-
+    
+    
     // Implement insert to handle requests to insert a single new row of data
     @Override
-    public Uri insert(@NonNull Uri uri, ContentValues values) {
+    public Uri insert(@NonNull Uri uri, ContentValues values)
+    {
         // Get access to the task database (to write new data to)
         final SQLiteDatabase db = mTaskDbHelper.getWritableDatabase();
-
+        
         // Write URI matching code to identify the match for the tasks directory
         int match = sUriMatcher.match(uri);
         Uri returnUri; // URI to be returned
-
-        switch (match) {
+        
+        switch (match)
+        {
             case TASKS:
                 // Insert new values into the database
                 // Inserting values into tasks table
                 long id = db.insert(TABLE_NAME, null, values);
-                if ( id > 0 ) {
+                if (id > 0)
+                {
                     returnUri = ContentUris.withAppendedId(TaskContract.TaskEntry.CONTENT_URI, id);
-                } else {
+                }
+                else
+                {
                     throw new android.database.SQLException("Failed to insert row into " + uri);
                 }
                 break;
@@ -106,51 +115,66 @@ public class TaskContentProvider extends ContentProvider {
             default:
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
         }
-
+        
         // Notify the resolver if the uri has been changed, and return the newly inserted URI
         getContext().getContentResolver().notifyChange(uri, null);
-
+        
         // Return constructed uri (this points to the newly inserted row of data)
         return returnUri;
     }
-
-
+    
+    
     // Implement query to handle requests for data by URI
     @Override
     public Cursor query(@NonNull Uri uri, String[] projection, String selection,
-                        String[] selectionArgs, String sortOrder) {
-
+                        String[] selectionArgs, String sortOrder)
+    {
+        
         // TODO (1) Get access to underlying database (read-only for query)
-
+        SQLiteDatabase db = mTaskDbHelper.getReadableDatabase();
         // TODO (2) Write URI match code and set a variable to return a Cursor
-
-        // TODO (3) Query for the tasks directory and write a default case
-
+        Cursor resultCursor;
+        switch (sUriMatcher.match(uri))
+        {
+            // TODO (3) Query for the tasks directory and write a default case
+            case TASKS:
+                resultCursor = db.query(TaskContract.TaskEntry.TABLE_NAME,
+                        projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+                
+            default:
+                throw new UnsupportedOperationException("Unknown URI: " + uri);
+        }
+        
         // TODO (4) Set a notification URI on the Cursor and return that Cursor
-
-        throw new UnsupportedOperationException("Not yet implemented");
+        resultCursor.setNotificationUri(getContext().getContentResolver(), uri);
+        
+        return resultCursor;
     }
-
-
+    
+    
     @Override
-    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs) {
-
+    public int delete(@NonNull Uri uri, String selection, String[] selectionArgs)
+    {
+        
         throw new UnsupportedOperationException("Not yet implemented");
     }
-
-
+    
+    
     @Override
     public int update(@NonNull Uri uri, ContentValues values, String selection,
-                      String[] selectionArgs) {
-
+                      String[] selectionArgs)
+    {
+        
         throw new UnsupportedOperationException("Not yet implemented");
     }
-
-
+    
+    
     @Override
-    public String getType(@NonNull Uri uri) {
-
+    public String getType(@NonNull Uri uri)
+    {
+        
         throw new UnsupportedOperationException("Not yet implemented");
     }
-
+    
 }
